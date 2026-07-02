6166b7a51263bb78373124db30fb9313bff32278
computersarecool1 <127455540+computersarecool1@users.noreply.github.com>
2026-07-02 00:37:13 -0400

    Create load_scene.rs

---
+++ src/load_scene.rs
@@ -0,0 +1,45 @@
+use std::process::Command;
+
+use bevy::{color::palettes::css::RED, prelude::*, sprite::Anchor};
+use image::codecs::pnm::ArbitraryTuplType::RGB;
+
+use crate::{Code, EnumCode};
+
+pub struct loadPlugin;
+
+impl Plugin for loadPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, load);        
+
+    }
+}
+#[derive(Component)]
+struct Load;
+fn load(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let mut i = 0;
+
+    let width = 0.3;
+    let height = 0.3;
+    let w = 110;
+    let h = 120;
+    for x in 1..w {     for y in 0..h { 
+
+let entit= commands.spawn((
+        Mesh2d(meshes.add(Rectangle::new(1.,1.))),
+        Transform::from_translation(Vec3 { x: 529. + x as f32 *width as f32*50 as f32, y: -130. + y as f32 *height as f32*50 as f32, z: -32. }).with_scale(Vec3 { x: width, y: height, z: 2.}),
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(Srgba::rgb(0.1*x as f32, 1.0, 0.0)))),
+        Load,Code{ s: EnumCode::MyKey((String::from(format!("[Text(\"{}\")]",(x+y*h)))))},
+    )).id();        let regular_font_handle: Handle<Font> = Default::default();
+commands.spawn((
+    Text2d::new(format!("{}",(x+y*h))),
+    TextColor(Color::WHITE),
+    TextFont::from(regular_font_handle.clone()).with_font_size(28.),
+    
+  ChildOf(entit),Anchor::TOP_RIGHT,));
+i += 1;
+    }}
+}
