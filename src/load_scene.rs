20b94238942ddb64b188fdd57e8bc93e5ff25834
computersarecool1 <127455540+computersarecool1@users.noreply.github.com>
2026-07-02 13:41:44 -0400

    18

--- src/load_scene.rs
+++ src/load_scene.rs
@@ -22,22 +22,22 @@ fn load(
 ) {
     let mut i = 0;
 
-    let width = 0.3;
-    let height = 0.3;
-    let w = 110;
-    let h = 120;
+    let width = 0.5;
+    let height = 0.5;
+    let w = 35;
+    let h = 30;
     for x in 1..w {     for y in 0..h { 
 
 let entit= commands.spawn((
         Mesh2d(meshes.add(Rectangle::new(1.,1.))),
-        Transform::from_translation(Vec3 { x: 529. + x as f32 *width as f32*50 as f32, y: -130. + y as f32 *height as f32*50 as f32, z: -32. }).with_scale(Vec3 { x: width, y: height, z: 2.}),
+        Transform::from_translation(Vec3 { x: 9. + y as f32 *width as f32*50 as f32, y: -530. + x as f32 *height as f32*50 as f32, z: -32. }).with_scale(Vec3 { x: width, y: height, z: 2.}),
         MeshMaterial2d(materials.add(ColorMaterial::from_color(Srgba::rgb(0.1*x as f32, 1.0, 0.0)))),
-        Load,Code{ s: EnumCode::MyKey((String::from(format!("[Text(\"{}\")]",(x+y*h)))))},
+        Load,Code{ s: EnumCode::MyKey((String::from(format!("[Text(\"{}\")]",(i)))))},
     )).id();        let regular_font_handle: Handle<Font> = Default::default();
 commands.spawn((
-    Text2d::new(format!("{}",(x+y*h))),
+    Text2d::new(format!("{}",(i))),
     TextColor(Color::WHITE),
-    TextFont::from(regular_font_handle.clone()).with_font_size(28.),
+    TextFont::from(regular_font_handle.clone()).with_font_size(24.),
     
   ChildOf(entit),Anchor::TOP_RIGHT,));
 i += 1;
